d5365eab4da54dcca51c2122634adaa3f0046c06
azer <azer.xu@gmail.com>
2023-05-17 20:13:00 +0800

    add init-git to setting git

---
+++ init-lisp/init-git.el
@@ -0,0 +1,33 @@
+;;; Package: init-git.el --- Summary
+;;; Commentary: init git
+;;; Code:
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; (use-package evil-magit
+;;   :after magit)
+
+;; Add a super-convenient global binding for magit-status since
+(global-set-key (kbd "C-M-;") 'magit-status)
+
+(dw/leader-key-def
+ "g"    '(:ignore t :which-key "git")
+ "gm"   'magit-commit
+ "gs"   'magit-status
+ "gd"   'magit-diff-unstaged
+ "gc"   'magit-branch-or-checkout
+ "gl"   '(:ignore t :which-key "log")
+ "glc"  'magit-log-current
+ "glf"  'magit-log-buffer-file
+ "gb"   'magit-branch
+ "gP"   'magit-push-current
+ "gp"   'magit-push-branch
+ "gf"   'magit-fetch
+ "gF"   'magit-fetch-all
+ "gr"   'magit-rebase)
+
+(provide 'init-git)
+;;; init-git ends here
