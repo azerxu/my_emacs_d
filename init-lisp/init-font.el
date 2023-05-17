0e7362b8f37972afbb70c189a46e54b8f9f23684
azer <azer.xu@gmail.com>
2023-05-17 20:12:19 +0800

    add init-font to setting font

---
+++ init-lisp/init-font.el
@@ -0,0 +1,14 @@
+;;; Package: init-font.el --- Summary
+;;; Commnentary: Font setting
+;;; Code:
+
+;; 使用Mac Font
+;; (set-face-attribute 'default nil :font "MesloLGSDZ Nerd Font Mono" :height 120)
+
+;; 使用Ubuntu Mono Font
+(set-face-attribute 'default nil :font "UbuntuMono Nerd Font Mono" :height 144)
+
+
+
+(provide 'init-font)
+;;; init-font.el ends here
