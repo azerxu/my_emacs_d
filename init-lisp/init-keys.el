a5520ba551b98cdeeaaf14c4d46eff9b771b8d5c
azer <azer.xu@gmail.com>
2023-05-18 15:16:49 +0800

    add oa/oc to open org-agenda and org-capture,add C-S-o to add a new line below

--- init-lisp/init-keys.el
+++ init-lisp/init-keys.el
@@ -9,6 +9,18 @@
 ;; 设置注释选择区域快捷键
 (global-set-key (kbd "M-o") 'comment-or-uncomment-region)
 
+
+
+(defun vi-open-line-below ()
+  "Insert a newline below the current line and put point at beginning."
+    (interactive)
+    (unless (eolp)
+      (end-of-line))
+    (newline-and-indent))
+
+(define-key global-map (kbd "C-S-o") 'vi-open-line-below)
+
+
 ;; 切换buffer带预览
 ;; (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 
@@ -31,13 +43,15 @@
 			  :prefix "C-x")
 
   (rune/leader-keys
-   "t"  '(:ignore t :which-key "toggles")
-   "tt" '(counsel-load-theme :which-key "choose theme")))
+    "o"  '(:ignore t :which-key "org")
+    "oa" '(org-agenda :which-key "org agenda")
+    "oc" '(org-capture :which-key "org capture")
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
 
 
 (general-define-key
  "C-M-j" 'counsel-switch-buffer)
-
 
 
 (provide 'init-keys)
