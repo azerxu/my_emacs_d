13338857881a734565d8d08bc6f8de566c08cc33
azer <azer.xu@gmail.com>
2023-05-18 15:18:38 +0800

    setting fonts

--- init-lisp/init-font.el
+++ init-lisp/init-font.el
@@ -8,6 +8,12 @@
 ;; 使用Ubuntu Mono Font
 (set-face-attribute 'default nil :font "UbuntuMono Nerd Font Mono" :height 144)
 
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "UbuntuMono Nerd Font Mono" :height 144)
+
+;; Set the variable pitch face
+;; (set-face-attribute 'variable-pitch nil :font "Cantarell" :height 192 :weight 'regular)
+(set-face-attribute 'variable-pitch nil :font "Sarasa Mono SC" :height 128 :weight 'regular)
 
 
 (provide 'init-font)
