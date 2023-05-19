8d3be4e8dd136c59ed44ca31edd18df3fe6b1310
azer <azer.xu@gmail.com>
2023-05-19 22:35:10 +0800

    fix icon too larger error

--- init-lisp/init-theme.el
+++ init-lisp/init-theme.el
@@ -4,6 +4,8 @@
 
 (use-package all-the-icons)
 
+(setq all-the-icons-scale-factor 0.8)
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
@@ -11,11 +13,6 @@
 
 (use-package doom-themes
   :init (load-theme 'doom-one t))
-
-(setq all-the-icons-scale-factor 2.1)
-;; setting theme
-;; (load-theme 'wombat t)
-
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
