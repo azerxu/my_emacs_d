3e842b5252f5c4abf88ce844ba1c9a39a54f648c
azer <azer.xu@gmail.com>
2023-05-19 18:47:49 +0800

    clean code by remove git-gutter

--- init-lisp/init-git.el
+++ init-lisp/init-git.el
@@ -29,18 +29,6 @@
  "gr"   'magit-rebase)
 
 
-;; (use-package git-gutter
-;;   :hook (prog-mode . git-gutter-mode)
-;;   :config
-;;   (setq git-gutter:update-interval 0.1))
-
-;; (use-package git-gutter-fringe
-;;   :config
-;;   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-;;   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-;;   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
-
-
 (use-package diff-hl
   :hook
   (prog-mode . siren-turn-on-diff-hl-mode)
