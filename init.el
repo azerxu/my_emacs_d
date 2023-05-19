9041cbebf8f81b21b911aebf82d60a39c2b01fbc
azer <azer.xu@gmail.com>
2023-05-19 18:45:33 +0800

    update

--- init.el
+++ init.el
@@ -21,18 +21,19 @@
 (require 'init-python)			; 设置python
 (require 'init-dired)			; 设置dired file manager
 
-(provide 'init)
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zoom-window dired-hide-dotfiles dired-open all-the-icons-dired dired-single python-mode use-package)))
+   '(diff-hl git-gutter-fringe git-gutter zoom-window dired-hide-dotfiles dired-open all-the-icons-dired dired-single python-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
