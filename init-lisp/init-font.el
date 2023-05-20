ba5985b0b2904c99a22595574ac3ec9d27ec1906
azer <azer.xu@gmail.com>
2023-05-21 03:51:40 +0800

    fix emacs daemon mode can not use font setting

--- init-lisp/init-font.el
+++ init-lisp/init-font.el
@@ -5,15 +5,50 @@
 ;; 使用Mac Font
 ;; (set-face-attribute 'default nil :font "MesloLGSDZ Nerd Font Mono" :height 120)
 
-;; 使用Ubuntu Mono Font
-(set-face-attribute 'default nil :font "UbuntuMono Nerd Font Mono" :height 144)
+;; ;; 使用Ubuntu Mono Font
+;; (set-face-attribute 'default nil
+;;                     :font "UbuntuMono Nerd Font Mono"
+;;                     :height 144)
 
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "UbuntuMono Nerd Font Mono" :height 144)
+;; ;; Set the fixed pitch face
+;; (set-face-attribute 'fixed-pitch nil
+;;                     :font "UbuntuMono Nerd Font Mono"
+;;                     :height 144)
 
-;; Set the variable pitch face
-;; (set-face-attribute 'variable-pitch nil :font "Cantarell" :height 192 :weight 'regular)
-(set-face-attribute 'variable-pitch nil :font "Sarasa Mono SC" :height 128 :weight 'regular)
+;; ;; Set the variable pitch face
+;; (set-face-attribute 'variable-pitch nil
+;;                     :font "Sarasa Mono SC"
+;;                     :weight 'regular
+;;                     :height 128)
+
+
+(defun efs/set-font-faces ()
+  (message "Setting faces!")
+  (set-face-attribute 'default nil
+                      :font "UbuntuMono Nerd Font Mono"
+                      :height 144)
+
+  ;; Set the fixed pitch face
+  (set-face-attribute 'fixed-pitch nil
+                      :font "UbuntuMono Nerd Font Mono"
+                      :height 144)
+
+  ;; Set the variable pitch face
+  (set-face-attribute 'variable-pitch nil
+                      :font "Sarasa Mono SC"
+                      :weight 'regular
+                      :height 128))
+
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (setq doom-modeline-icon t)
+                (with-selected-frame frame
+                  (efs/set-font-faces))))
+  (efs/set-font-faces))
+
+
 
 
 (provide 'init-font)
