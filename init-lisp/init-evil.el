6bfbd57127d2f206ac518b1c759e1be7553d42d9
azer <azer.xu@gmail.com>
2023-05-21 03:50:29 +0800

    add undo-tree and update some variable

--- init-lisp/init-evil.el
+++ init-lisp/init-evil.el
@@ -6,13 +6,13 @@
   (dolist (mode '(custom-mode
 		  eshell-mode
 		  git-rebase-mode
-		  erc-mode
-		  circe-server-mode
-		  circe-chat-mode
-		  circe-query-mode
-		  sauron-mode
 		  term-mode))
     (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
+
 
 ;; C-z to switch emacs and vim
 (use-package evil
@@ -21,8 +21,14 @@
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
-  :config
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-undo-system 'undo-tree)
   (evil-mode 1)
+  :config
+
+  ;; Rebind `universal-argument' to `C-M-u' since `C-u' now scrolls the buffer
+  (global-set-key (kbd "C-M-u") 'universal-argument)
+
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
   (define-key evil-normal-state-map (kbd "C-e") 'evil-end-of-line)
@@ -33,24 +39,15 @@
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-  ;; :hook (evil-mode . rune/evil-hook))
+  (evil-set-initial-state 'dashboard-mode 'normal)
+
+  :hook (evil-mode . efs/evil-hook))
+
 
 (use-package evil-collection
   :after evil
   :config
   (evil-collection-init))
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit 1))
-
-(efs/leader-key-def
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
 
 
 (provide 'init-evil)
