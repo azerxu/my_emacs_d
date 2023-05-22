49fc8c94bc23ac98f329796d49ec5dc73461121b
azer <azer.xu@gmail.com>
2023-05-23 07:52:05 +0800

    comment init.el

--- init.el
+++ init.el
@@ -8,25 +8,47 @@
 ;; Add init-lisp package directory to load-path for require loading package
 (add-to-list 'load-path (expand-file-name "init-lisp" user-emacs-directory))
 
+;; -------------------------------------------------------------------------
+;; Default Settings
+;; -------------------------------------------------------------------------
 (require 'init-font)                ; 设置字体
 (require 'init-ui)                  ; EMACS通用设置
 (require 'init-setting)             ; 一些有用的设置
-(require 'init-package)             ; 安装第三方包
+
+
+;; -------------------------------------------------------------------------
+;; Package Settings
+;; -------------------------------------------------------------------------
+(require 'init-package)             ; 设置Package Manager
+
+
+;; -------------------------------------------------------------------------
+;; Package Settings
+;; -------------------------------------------------------------------------
+(require 'init-theme)               ; 设置主题
 (require 'init-keys)                ; 设置自定义快捷键
 (require 'init-tools)               ; 设置通用工具
 (require 'init-evil)                ; 设置Evil-Mode兼容vim
-(require 'init-theme)               ; 设置主题
-(require 'init-completion)          ; completion
-(require 'init-git)                 ; 设置git
 (require 'init-projectile)          ; 设置projectile
-(require 'init-org)                 ; 设置org
-(require 'init-lsp)                 ; 设置lsp language server protocol
 (require 'init-term)                ; 设置vterm
 (require 'init-dired)               ; 设置dired file manager
+
+;; -------------------------------------------------------------------------
+;; Programming Settings
+;; -------------------------------------------------------------------------
+(require 'init-completion)          ; completion
+(require 'init-lsp)                 ; 设置lsp language server protocol
+(require 'init-git)                 ; 设置git
+
+;; Language Setting
 (require 'init-python)              ; 设置python
 (require 'init-lua)                 ; 设置lua
 
 
+;; -------------------------------------------------------------------------
+;; Org Setting
+;; -------------------------------------------------------------------------
+(require 'init-org)                 ; 设置org
 (require 'init-present)             ; show org as PPT
 
 
