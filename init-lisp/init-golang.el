95a0937c74889fd16a65e9c63acee48db5038845
azer <azer.xu@gmail.com>
2023-05-23 19:01:32 +0800

    update init-golang

--- init-lisp/init-golang.el
+++ init-lisp/init-golang.el
@@ -19,12 +19,12 @@
   :bind (:map go-mode-map
               ("C-c C-c" . go-run-buffer)
               ("C-c C-j" . godef-jump)
-			  ("C-c C-d" . godoc)
+	      ("C-c C-d" . godoc)
               ("<f6>" . gofmt))
   :config
   ;; auto gofmt
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook 'before-save-hook 'go-remove-unused-imports)
+
   (require 'lsp-go)
   (setq lsp-go-analyses
         '((fieldalignment . t)
@@ -38,24 +38,11 @@
   (setq indent-tabs-mode 1))
 
 
-;; install init-go
-;; (use-package go-autocomplete)
-;; (require 'go-autocomplete)
-
-
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/lisp/ac-dict")
-;; (add-to-list 'ac-modes 'go-mode)
-;; (ac-config-default)
-
 ;; go-guru setting
 ;; (require 'go-guru)
 ;; (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
 
 ;; (add-hook 'go-mode-hook 'go-eldoc-setup)
-
-;; (add-hook 'go-mode-hook (lambda ()
-;;                           (set (make-local-variable 'company-backends) '(company-go))
-;;                           (company-mode)))
 
 ;; (add-hook
 ;;  'go-mode-hook
@@ -67,27 +54,13 @@
 
 ;;    ;;; (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 
-;;    ;; Use goimports instead of go-fmt
-;;    (setq gofmt-command "goimports")
-
 ;;    ;; Customize compile command to run go build
 ;;    (if (not (string-match "go" compile-command))
 ;;        (set (make-local-variable 'compile-command)
 ;;             "go generate && go build -v && go test -v && go vet"))
 
-;;    ;; auto gofmt
-;;    (add-hook 'before-save-hook 'gofmt-before-save)
-;;    ;; (add-hook 'before-save-hook 'go-remove-unused-imports)
-;;    (setq tab-width 4)
+;    (setq tab-width 4)
 ;;    (setq indent-tabs-mode 1)))
-
-;; godef setting
-; (defun my-go-mode-hook ()
-;   ; Call Gofmt before saving
-;   (add-hook 'before-save-hook 'gofmt-before-save)
-;   ; Godef jump key binding
-;   (local-set-key (kbd "M-.") 'godef-jump))
-;  (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (provide 'init-golang)
 ;;; init-golang.el ends here
