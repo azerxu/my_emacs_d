2b4d7ebc37a7d1cfcf4a2281f0961912a8d4d313
azer <azer.xu@gmail.com>
2023-05-23 20:38:48 +0800

    add python exec and python run buffer

--- init-lisp/init-python.el
+++ init-lisp/init-python.el
@@ -2,9 +2,18 @@
 ;;; Commnentary: Emacs setting for Python
 ;;; Code:
 
+(defun python-run-buffer()
+  (interactive)
+  (shell-command (concat "python3 " (buffer-name))))
+
+
 (use-package python-mode
   :ensure t
   :hook (python-mode . lsp-deferred)
+  :bind (:map python-mode-map
+              ("C-c C-c" . python-run-buffer))
+  :config
+  (add-to-list 'exec-path "/home/azer/.local/bin")
   :custom
   (python-shell-interpreter "python3"))
 
