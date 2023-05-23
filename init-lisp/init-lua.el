ebe723a5ab271ebb5fe4bda360f779836933abca
azer <azer.xu@gmail.com>
2023-05-23 22:56:54 +0800

    fix vim not defined error

--- init-lisp/init-lua.el
+++ init-lisp/init-lua.el
@@ -2,9 +2,22 @@
 ;;; Commnentary:
 ;;; Code:
 
+
+(use-package lsp-lua
+  :ensure nil
+  :diminish
+  :config
+  (setq lsp-lua-diagnostics-globals '("vim"))
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-lua)
+                         (lsp-deferred))))
+
 (use-package lua-mode
   :ensure t
+  :diminish
   :hook (lua-mode . lsp-deferred))
+
+(use-package )
 
 
 
