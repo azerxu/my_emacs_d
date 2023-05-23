10a25adb5d4e1f5e406a76dc345e7c1abe61911c
azer <azer.xu@gmail.com>
2023-05-23 20:48:36 +0800

    fix tab select and add key binding

--- init-lisp/init-lsp.el
+++ init-lisp/init-lsp.el
@@ -10,25 +10,38 @@
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :hook (lsp-mode . efs/lsp-mode-setup)
+  :bind (:map lsp-mode-map
+              ;; ("C-c l" . lsp-command-map)
+              ("C-c d" . lsp-describe-thing-at-point)
+              ("C-c a" . lsp-execute-code-action))
   :init
   (setq lsp-keymap-prefix "C-c l")	; or "C-l", "S-l"
+  (setq lsp-enable-snippet nil)
   :config
   (lsp-enable-which-key-integration t))
 
 
 (use-package company
   :after lsp-mode
-  :hook (lsp-mode . company-mode)
+  :hook ((lsp-mode . company-mode)
+         (emacs-lisp-mode . (lambda ()
+                              (setq-local company-backends '(company-elisp))))
+         (emacs-lisp-mode . company-mode))
   :bind
   (:map company-active-map
-	("<tab>" . company-complete-selection)
-	("C-l" . company-complete-selection))
+        ("<tab>" . company-select-next-if-tooltip-visible-or-complete-selection)
+        ;; ("<tab>" . company-complete-selection)
+        ("C-l" . company-complete-selection))
   (:map lsp-mode-map
         ("<tab>" . company-indent-or-complete-common))
+  :config
+  (company-keymap--unbind-quick-access company-active-map)
+  ;; (company-tng-mode)
+  (company-tng-configure-default)
   :custom
   (company-show-numbers t)
   (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
+  (company-idle-delay 0.1))
 
 
 (use-package company-box
@@ -52,6 +65,8 @@
 ;;   (add-to-list 'company-backends #'company-tabnine))
 
 ;; ;; (require 'company-tabnine)
+
+(use-package flycheck)
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
