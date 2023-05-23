f0155e054189b34cdb4fb9797a7a8150b79cf72b
azer <azer.xu@gmail.com>
2023-05-23 22:56:02 +0800

    fix completion error

--- init-lisp/init-python.el
+++ init-lisp/init-python.el
@@ -2,21 +2,45 @@
 ;;; Commnentary: Emacs setting for Python
 ;;; Code:
 
+;; ************************************************************
+;; python setting
+;; ************************************************************
+(setenv "PATH" (concat (getenv "PATH") ":/home/azer/.local/bin"))
+
+
 (defun python-run-buffer()
   (interactive)
   (shell-command (concat "python3 " (buffer-name))))
+
+
+(use-package lsp-pylsp
+  :ensure nil
+  :config
+  (add-to-list 'exec-path "/home/azer/.local/bin")
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pylsp)
+                         (lsp-deferred))))
+
+
+;; (use-package lsp-pyright
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'exec-path "/home/azer/.local/bin")
+;;   ;; (setq lsp-pylsp-server-command "/home/azer/.local/bin/pylsp")
+;;   :hook (python-mode . (lambda ()
+;;                          (require 'lsp-pyright)
+;;                          (lsp-deferred))))
 
 
 (use-package python-mode
   :ensure t
   :hook (python-mode . lsp-deferred)
   :bind (:map python-mode-map
-              ("C-c C-c" . python-run-buffer))
+              ("C-c S-c" . python-run-buffer))
   :config
   (add-to-list 'exec-path "/home/azer/.local/bin")
   :custom
   (python-shell-interpreter "python3"))
-
 
 
 (provide 'init-python)
