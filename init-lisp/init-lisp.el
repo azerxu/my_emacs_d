9310b8f5c2ef377e341f847bd5b01a7d7ab5025b
azer <azer.xu@gmail.com>
2023-05-24 17:08:51 +0800

    add company-mode in elisp-mode

--- init-lisp/init-lisp.el
+++ init-lisp/init-lisp.el
@@ -2,7 +2,14 @@
 ;;; Commnentary:
 ;;; Code:
 
-;; (add-hook 'emacs-lisp-mode-hook 'company-mode)
+(use-package emacs-lisp-mode
+  :ensure nil
+  :config
+  :hook ((emacs-lisp-mode . (lambda ()
+                              (setq-local company-backends '(company-elisp))))
+         (emacs-lisp-mode . company-mode)))
+
+
 
 (provide 'init-lisp)
 ;;; init-lisp.el ends here
