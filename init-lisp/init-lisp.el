3731f84392da78de8df2c7d8400e80c2ea780e02
azer <azer.xu@gmail.com>
2023-05-26 00:19:58 +0800

    add motion key binding

--- init-lisp/init-lisp.el
+++ init-lisp/init-lisp.el
@@ -5,6 +5,20 @@
 (use-package emacs-lisp-mode
   :ensure nil
   :config
+  (general-def
+    :prefix ","
+    :states 'motion
+    :keymaps 'emacs-lisp-mode-map
+    ""    nil
+    "e"  '(nil :which-key "eval")
+    "es" '(eval-last-sexp :which-key "eval last sexp")
+    "er" '(eval-region :which-key "eval region")
+    "eb" '(eval-buffer :which-key "eval buffer")
+
+    "c"  '(check-parens :which-key "check parens")
+    "I"  '(indent-region :which-key "indent region")
+    )
+  
   :hook ((emacs-lisp-mode . (lambda ()
                               (setq-local company-backends '(company-elisp))))
          (emacs-lisp-mode . company-mode)))
